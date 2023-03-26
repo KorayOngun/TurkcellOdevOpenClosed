f1f1efe5f65933989c82215e65635191687eb32c
koray öngün <koraygg28@gmail.com>
2023-03-26 20:16:47 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿namespace TurkcellOdevOpenClosed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculating the area of shapes
+
+            IShape[] IShapes = {
+                new Triangle(10, 10),
+                new Square(0.60),
+                new Rectangle(10, 20),
+                new Circle(4)
+            };
+
+            foreach (IShape IShape in IShapes)
+                Console.WriteLine(IShape.Area);
+        }
+    }
+}
+
+
+
+interface IShape
+{
+    double Area();
+}
+
+
+class Triangle : IShape
+{
+    public Triangle(double _base, double _height)
+    {
+        Base = _base; Height = _height;
+    }
+    private double Base { get; set; }
+    private double Height { get; set; }
+    public double Area()
+    {
+        return Height * Base * 0.5;
+    }
+}
+class Square : IShape
+{
+    public Square(double _base)
+    {
+        Base = _base;
+    }
+
+    private double Base { get; set; }
+
+    public double Area()
+    {
+        return Base * Base;
+    }
+}
+class Rectangle : IShape
+{
+
+    public Rectangle(double _width, double _height)
+    {
+        Width = _width;
+        Height = _height;
+    }
+
+    public double Area()
+    {
+        return Width * Height;
+    }
+
+    private double Width { get; set; }
+    private double Height { get; set; }
+}
+
+class Circle : IShape
+{
+    public Circle(double _radius)
+    {
+        radius = _radius;
+    }
+
+    public double Area()
+    {
+        return radius * radius * Math.PI;
+    }
+
+    private double radius { get; set; }
+
+
+}
